cc958cc89c124dac509ea62b67d8218bf8b8d2a4
HumamHo44 <humaminof@gmail.com>
2025-03-13 05:34:00 +0300

    Done

--- session_2/q9.dart
+++ session_2/q9.dart
@@ -10,41 +10,25 @@ void main() {
   int? number = int.tryParse(stdin.readLineSync() ?? "");
 
   switch (number) {
+    case 12:
     case 1:
-      print('January');
-      break;
     case 2:
-      print("February");
+      print("Winter");
       break;
     case 3:
-      print("March");
-      break;
     case 4:
-      print("April");
-      break;
     case 5:
-      print("May");
+      print("Spring");
       break;
     case 6:
-      print("June");
-      break;
     case 7:
-      print("July");
-      break;
     case 8:
-      print("August");
+      print("Summer");
       break;
     case 9:
-      print("September");
-      break;
     case 10:
-      print("October");
-      break;
     case 11:
-      print("November");
-      break;
-    case 12:
-      print("December");
+      print("Autumn");
       break;
   }
 }
